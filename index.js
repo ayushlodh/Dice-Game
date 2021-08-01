349921857b3ae0a33e1cf762d21c4212badbd242
ayushlodh <87244116+ayushlodh@users.noreply.github.com>
2021-08-01 14:22:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelector("button").addEventListener("click", handleClick);
+function handleClick() {
+  var randomNumber1 = Math.floor(Math.random()*6)+1;
+  var randonDiceImage = "dice"+randomNumber1+".png";
+  var randomImageSource = "images/"+randonDiceImage;
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomImageSource);
+  var randomNumber2 = Math.floor(Math.random()*6)+1;
+  var randonDiceImage2 = "dice"+randomNumber2+".png";
+  var randomImageSource2 = "images/"+randonDiceImage2;
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImageSource2);
+  if(randomNumber1 > randomNumber2)
+  {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+  }
+  else if(randomNumber1 < randomNumber2)
+  {
+    document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+  }
+  else
+  {
+    document.querySelector("h1").innerHTML = "🚩Draw!";
+  }
+}
